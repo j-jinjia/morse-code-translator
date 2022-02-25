7f53a745c863817760422019e1b53bf550dae42a
j-jinjia <j.jinjia99@gmail.com>
2022-02-25 15:32:33 +0100

    update tests

--- script.test.js
+++ script.test.js
@@ -11,16 +11,19 @@ describe("translate words from english to morse", () => {
   // positive tests for a single letter
   it("b should translate correctly to morse code", () => {
     const result = translateEnglishToMorse("b");
-    expect(result).toBe("");
+    expect(result).toBe("-...");
   });
   // positive tests for multiple words
   it("walk should translate correctly to morse code", () => {
     const result = translateEnglishToMorse("walk talk");
-    expect(result).toBe("");
+    expect(result).toBe(".-- .- .-.. -.- - .- .-.. -.-");
   });
 
   // negative tests for edge cases
-
+  it("+ sign should not translate and return invalid text", () =>{
+      const result = translateEnglishToMorse("+");
+      expect(result).toBe("invalid");
+  });
 });
 
 //describe("translates morse code to english", () => {});
