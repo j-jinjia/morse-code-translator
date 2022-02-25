47c8de51499973cb99730b7892a313455557cc62
j-jinjia <j.jinjia99@gmail.com>
2022-02-25 14:01:47 +0100

    text to morse translator

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import morseCode from "./morseobject.js"
+import {translateEnglishToMorse} from "./functions.js"
 /**  Morse code pseudo code
  *  1.- user input in input field
  *  2.- Event listeners on input field for each letter
@@ -13,21 +13,7 @@ import morseCode from "./morseobject.js"
 const inputMorse = document.querySelector(".input");
 const outputMorse = document.querySelector(".output");
 
-//functions
-// sperates each letter of the input word. 
-const translateEnglishToMorse = () =>{
-    const letterArr = inputMorse.value.split("")
-    console.log(letterArr);
-    console.log(Object.values(morseCode));
-    //console.log(Object.keys(morseCode)) ;
-    letterArr.forEach(element => {
-        console.log(element);
-        if (element == Object.keys(morseCode)){
-            return (Object.values(morseCode));
-        }
-        
-    });
-};
+
 
 //eventlisteners
 
