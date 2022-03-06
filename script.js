f82405e07c1e671e82917fc84f85e97e1e574106
j-jinjia <j.jinjia99@gmail.com>
2022-03-06 23:22:29 +0100

    morse-english english-morse translateor working

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-import { translateEnglishToMorse } from "./functions.js";
+import { translateEnglishToMorse } from "./src/functions.js";
+import { translateMorseToEnglish } from "./src/functions.js";
 //DOM
 const englishInput = document.querySelector("#english-word");
 const morseTranslateResult = document.querySelector("#morse-translate-result");
@@ -11,16 +12,23 @@ const handleInput = () => {
   const inputValue = englishInput.value;
   return inputValue
 };
-//Calls the translate function in function.js with the value of input text
+//Calls the translate function in function.js with the value of input text and returns the english equivalent
 const handleTranslationMorse =() => {
   const result = translateEnglishToMorse(handleInput());
   morseTranslateResult.innerText = result;
 }
-
+//calls the translate to english function from functions.js with the value of input text and returns the morse equivalent.
+const translateEnglishButton = document.querySelector("#translate-english-button");
+const handletranslationEnglish =()=>{
+  const result = translateMorseToEnglish(handleInput());
+  console.log(result)
+  morseTranslateResult.innerText = result;
+} 
 //Clears input field and output result
 const handleClear =()=>{
   englishInput.value="";
   morseTranslateResult.innerText="Output translation"
+  englishInput.focus()
 }
 
 
@@ -28,18 +36,5 @@ const handleClear =()=>{
 englishInput.addEventListener("input", handleInput);
 translateMorseButton.addEventListener("click", handleTranslationMorse);
 clearButton.addEventListener("click", handleClear);
+translateEnglishButton.addEventListener("click",handletranslationEnglish)
 
-
-/**  Morse code pseudo code
- *  1.- user input in input field
- *  2.- Event listeners on input field 
- *  3.- Store the translation in an object for each letter. So const translation {A: bla, b: bla......}
- *      3.5.- Calling each object with forEach loop.
- *  4.- If letter x in input bar, return the value of x. 
- *  5.- Display on output translation screen. 
- * 
- * NOTES: Code works if everything is in the same file. Need to be separated.
- * Can't export as default more than 1 {solved}
- * add stylings
- * Code depends on DOM elements, need to make translateEnglishToMorse independent.
-*/
