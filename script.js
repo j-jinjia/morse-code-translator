a3fca7b73a65824710190b3ca3d3b35e515ba5c9
j-jinjia <j.jinjia99@gmail.com>
2022-03-06 19:50:29 +0100

    english-morse code works

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 import { translateEnglishToMorse } from "./functions.js";
+//DOM
+const englishInput = document.querySelector("#english-word");
+const morseTranslateResult = document.querySelector("#morse-translate-result");
+const translateMorseButton = document.querySelector("#translate-morse-button");
+const clearButton = document.querySelector("#clear-button");
+
+//FUNCTIONS
+// Gets the value of input text
+const handleInput = () => {
+  const inputValue = englishInput.value;
+  return inputValue
+};
+//Calls the translate function in function.js with the value of input text
+const handleTranslationMorse =() => {
+  const result = translateEnglishToMorse(handleInput());
+  morseTranslateResult.innerText = result;
+}
+
+//Clears input field and output result
+const handleClear =()=>{
+  englishInput.value="";
+  morseTranslateResult.innerText="Output translation"
+}
+
+
+//EVENT LISTENERS
+englishInput.addEventListener("input", handleInput);
+translateMorseButton.addEventListener("click", handleTranslationMorse);
+clearButton.addEventListener("click", handleClear);
+
+
 /**  Morse code pseudo code
  *  1.- user input in input field
- *  2.- Event listeners on input field for each letter
+ *  2.- Event listeners on input field 
  *  3.- Store the translation in an object for each letter. So const translation {A: bla, b: bla......}
  *      3.5.- Calling each object with forEach loop.
  *  4.- If letter x in input bar, return the value of x. 
@@ -12,15 +43,3 @@ import { translateEnglishToMorse } from "./functions.js";
  * add stylings
  * Code depends on DOM elements, need to make translateEnglishToMorse independent.
 */
-
-//global variables 
-//DOM
-const inputMorse = document.querySelector(".input");
-const outputMorse = document.querySelector(".output");
-const translateButton = document.querySelector(".translate")
-
-//eventlisteners
-translateButton.addEventListener("click",translateEnglishToMorse);
-
-
-export {inputMorse, outputMorse}
